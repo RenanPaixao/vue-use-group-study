39de86e233e393179fa8e7e8252d40092bb709a7
= <=>
2022-08-07 21:36:34 -0300

    add tailwind and flowbite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import unocss from "unocss/vite";
+import presetIcons from '@unocss/preset-icons/browser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    unocss({ 
+      presets: [
+        presetIcons({
+            cdn: 'https://esm.sh/'
+        })
+      ]
+    })
+  ]
 })
